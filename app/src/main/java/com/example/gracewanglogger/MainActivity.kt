e7a03912b607a857982a85e49df2c5b43b1f83dd
Grace Wang <grace25@bu.edu>
2024-02-15 14:55:05 -0500

    completed

--- app/src/main/java/com/example/gracewanglogger/MainActivity.kt
+++ app/src/main/java/com/example/gracewanglogger/MainActivity.kt
@@ -2,12 +2,60 @@ package com.example.gracewanglogger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log // Import statement for Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "ActivityLifecycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val text = "Hello World!"
+
+            // Set the "Hello World" text on both TextView and EditText
+            textView.text = text
+            editText.setText(text)
+        }
+        Log.d(TAG, "onCreate() called") // Log statement for onCreate
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
